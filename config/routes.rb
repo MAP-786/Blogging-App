454d382b6bcd94e681818858f5d51fc45d890a1b
Mohsin <mohsinpanjwani5@gmail.com>
2023-06-26 09:06:19 -0400

    everything works except delete

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
+
 Rails.application.routes.draw do
   root "articles#index"
 
-  get "/articles", to: "articles#index"
+  resources :articles do
+    resources :comments
+  end
 end
 
 Rails.application.routes.draw do
